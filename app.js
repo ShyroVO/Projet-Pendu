f0460278f4daf99a8f74f1ae0a82bbdc7032df8f
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-16 16:23:52 +0100

    0.2 // Bouton

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
-let dictionnaire = [ "CHEVAL", "PONEY", "ZONEZ" ]
-    let nombreDeMots = dictionnaire.length;
+let dictionnaire = ["ARMOIRE", "CHEVAL", "JEROME", "PONEY", "ZONEZ"]
 
+// random Words:
+function getRandom(){
+    return Math.floor(Math.random() * dictionnaire.length);
+}
+
+let randomWord = dictionnaire[getRandom()];
+
+for () {
+
+    let letters = document.createElement('button');
+    document.getElementById('penduLetters').appendChild(letters);
+
+}
+
+// Valeur buttons:
+let valeurButton = document.getElementsByClassName('lettersBtn');
+
+// Letters utilisé Affichage:
+for (let valeursButtons of valeurButton) {
+
+
+    valeursButtons.addEventListener('click', function () {
+        document.getElementById('console').innerHTML += " " + valeursButtons.innerHTML + " - ";
+
+        let valeurSearch = "'" + valeursButtons + "'";
+
+        if (randomWord.includes(valeurSearch)){
+
+
+        }
+
+
+
+
+
+
+    })
+
+}
