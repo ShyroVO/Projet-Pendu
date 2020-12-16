2f47b50e3bfeb46670d2bd27e39cf5b890fbf185
ShyroVO <Bosquet.laurie@outlook.com>
2020-12-16 17:01:48 +0100

    0.5 Mot random et case

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ function getRandom(){
 }
 
 let randomWord = dictionnaire[getRandom()];
+let withWord = randomWord.length;
 
-for () {
+console.log(withWord);
 
+for (let i = 0; i < withWord; i++) {
     let letters = document.createElement('button');
+
     document.getElementById('penduLetters').appendChild(letters);
+    letters.innerHTML = "_";
+    let classe = "'" + i + "'";
+    letters.classList.add(classe);
 
 }
 
@@ -20,15 +26,20 @@ let valeurButton = document.getElementsByClassName('lettersBtn');
 // Letters utilisé Affichage:
 for (let valeursButtons of valeurButton) {
 
-
     valeursButtons.addEventListener('click', function () {
+        console.log(randomWord);
+
+
+
+
         document.getElementById('console').innerHTML += " " + valeursButtons.innerHTML + " - ";
+        document.getElementsByClassName(1);
 
-        let valeurSearch = "'" + valeursButtons + "'";
-
-        if (randomWord.includes(valeurSearch)){
+        
 
 
+        if (randomWord.includes("'" + valeursButtons + "'")){
+            console.log("La lettre ce trouve dans le mot");
         }
 
 
